fc7174e349d73d48d5bb16a06d61513bf0f0226b
dongsubak <blackxub@naver.com>
2017-11-25 21:32:44 +0900

    Raw source files

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Movie from './Movie'
+
+class App extends Component {
+  state = {
+  }
+
+  componentDidMount(){
+    this._getMovies();
+    /*
+    fetch('https://yts.ag/api/v2/list_movies.json?sort_by=rating')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err)); */
+
+    /* setTimeout(()=>{
+      this.setState({
+        movies: [
+          {
+            title:"Matrix",
+            poster:"https://upload.wikimedia.org/wikipedia/en/c/c1/The_Matrix_Poster.jpg"
+          },
+          {
+            title:"Oldboy",
+            poster:"https://images-na.ssl-images-amazon.com/images/M/MV5BMTI3NTQyMzU5M15BMl5BanBnXkFtZTcwMTM2MjgyMQ@@._V1_UY1200_CR90,0,630,1200_AL_.jpg"
+          }
+        ]
+      })
+    }, 1000) */
+  }
+
+  _renderMoives = () => {
+    const movies = this.state.movies.map(movie => {
+      return <Movie
+        title={movie.title}
+        poster={movie.medium_cover_image}
+        key={movie.id}
+        genres={movie.genres}
+        synopsis={movie.synopsis}
+        />
+    })
+    return movies;
+  }
+
+  _getMovies = async () => {
+      const movies = await this._callApi();
+      this.setState({ movies });
+  }
+
+  _callApi = () => {
+    return fetch("https://yts.am/api/v2/list_movies.json?sort_by=download_count")
+    // return fetch('http://www.omdbapi.com/?i=tt3896198&apikey=9e9350f4&page=1')
+    .then(response => response.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err));
+  }
+
+
+  // callApi 끝날때까지 기다린다. movies 설정을. 그래서 setState로 callApi가 완료되기를 기다린다.
+
+  render() {
+    return (
+      <div className={this.state.movies ? "App" : "App--loading"}>
+         {this.state.movies ? this._renderMoives() : 'Loading'}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/Movie.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import LinesEllipsis from 'react-lines-ellipsis';
+import './Movie.css';
+
+
+
+class Movie extends Component {
+  render() {
+    console.log(this.props);
+    return (
+      <div className="Movie">
+        <div className="Movie__Columns">
+          <MoviePoster poster={this.props.poster} alt={this.props.title}/>
+        </div>
+        <div className="Movie__Columns">
+            <h1> {this.props.title} </h1>
+            <div className="Movie__Genres">
+              {this.props.genres.map((genre, index)=> <MovieGenre genre={genre} key={index} />)}
+            </div>
+            <div className="Movie__Synopsis">
+              <LinesEllipsis
+                text={this.props.synopsis}
+                maxLine='3'
+                ellipsis='...'
+                trimRight
+                basedOn='letters'
+              />
+            </div>
+        </div>
+
+      </div>
+    );
+  }
+}
+
+class MoviePoster extends Component{
+  render() {
+    return (
+      <img src={this.props.poster} alt={this.props.alt} title={this.props.alt} className="Movie__Poster"/>
+    );
+  }
+}
+
+
+//dumb Component - no props, no state 위에 class MoviePoster와 같아.
+function MovieGenre({genre}){
+  return (
+    <span className="Movie__Genres">{genre}</span>
+  );
+}
+
+Movie.propTypes = {
+    title: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired,
+    genres: PropTypes.array.isRequired,
+    synopsis: PropTypes.string.isRequired
+};
+
+MoviePoster.propTypes = {
+    poster: PropTypes.string.isRequired,
+    alt: PropTypes.string.isRequired
+};
+MovieGenre.propTypes = {
+    poster: PropTypes.string.isRequired
+};
+
+export default Movie;
